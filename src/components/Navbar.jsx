77554df448c632de75f5f60d599b188d05d96a75
iohanalopes <iohanalopes@alu.ufc.br>
2025-07-03 23:44:44 -0300

    Componente da Barra de Navegação

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,17 @@
-// Aqui vai a logo, o nome da pagina e os botoes
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2 className="navbar__titulo">Gerenciar Reservas</h2>
+      <div className="navbar__logo">
+        <img src="/logo-comodoro.png" alt="Logo Comodoro" className="logo" />
+      </div>
+      <div className="navbar__botoes">
+        <button className="btn-vermelho">Início</button>
+        <button className="btn-vermelho">Quartos</button>
+      </div>
+    </nav>
+    
+  );
+}
