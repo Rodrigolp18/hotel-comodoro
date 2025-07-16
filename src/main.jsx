817b5c0151618d6cffc4928cd3a08cef5eebfa4b
Emanoela-Maciel <142356529+Emanoela-Maciel@users.noreply.github.com>
2025-07-16 00:15:35 +0000

    Instalando react-router-dom e implementando as rotas no main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-import GerenciarReservas from './pages/GerenciarReservas.jsx'
+import GerenciarReservas from './pages/GerenciarReservas.jsx';
+import CadastrarHospedes from './pages/CadastrarHospedes.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <GerenciarReservas />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CadastrarHospedes />} />
+        <Route path="/gerenciar-reservas" element={<GerenciarReservas />} />
+        <Route path="/cadastrar-hospedes" element={<CadastrarHospedes />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
